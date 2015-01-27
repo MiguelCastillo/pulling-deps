5a081423bb2d9381ea3bc876a9b0241e96e26696
Miguel Castillo <manchagnu@gmail.com>
2015-01-27 00:28:20 -0500

    Adding logic to extract dependencies from acorn AST

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 /**
- * Empty module
+ * Module to extract dependencies from define and require statments
  */
 (function() {
   'use strict';
 
-  function SomePandaBear() {
-  }
 
-
-  SomePandaBear.prototype.eats = function() {
-    return 'Bamboo';
+  var TokenTypes = {
+    _define         : 'define',
+    _require        : 'require',
+    Identifier      : 'Identifier',
+    Literal         : 'Literal',
+    ArrayExpression : 'ArrayExpression'
   };
 
 
-  module.exports = SomePandaBear;
+  var acorn = require('acorn'),
+      walk  = require('acorn/util/walk');
+
+  function parse(source, options) {
+    var result = {dependencies: []};
+    var ast = acorn.parse(source, options);
+
+    walk.simple(ast, {
+      'CallExpression': function callExpression(node) {
+        if (isName(node.callee, TokenTypes._require)) {
+          if (isDependencyString(node.arguments)) {
+            result.dependencies.push(node.arguments[0].value);
+          }
+        }
+        else if (isName(node.callee, TokenTypes._define)) {
+          var dependencies = getDependencyArray(node.arguments);
+          if (dependencies && dependencies.length) {
+            result.dependencies = result.dependencies.concat(dependencies);
+          }
+        }
+      }
+    });
+
+    return result;
+  }
+
+
+  function isName(node, name) {
+    return TokenTypes.Identifier === node.type && name === node.name;
+  }
+
+
+  function isDependencyString(node) {
+    return node.length === 1 && TokenTypes.Literal === node[0].type;
+  }
+
+
+  function getDependencyArray(nodes) {
+    var elements, i, length;
+    if (nodes[0] && nodes[0].type === TokenTypes.ArrayExpression) {
+      elements = nodes[0].elements;
+    }
+    else if (nodes[1] && nodes[1].type === TokenTypes.ArrayExpression) {
+      elements = nodes[1].elements;
+    }
+
+    if (elements) {
+      for (i = 0, length = elements.length; i < length; i++) {
+        elements[i] = elements[i].value;
+      }
+    }
+
+    return elements;
+  }
+
+
+  module.exports = parse;
 })();
