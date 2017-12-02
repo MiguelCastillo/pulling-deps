763bbd0018fa5afdf9532fa29979251937567a0c
Miguel Castillo <manchagnu@gmail.com>
2017-12-02 15:36:22 -0500

    revamped unit tests to use mocha directly. also disable jshint as we transition to eslint

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -1,14 +1,1 @@
-define([
-  "chai"
-], function(chai) {
-
-  window.chai   = chai;
-  window.expect = chai.expect;
-  window.assert = chai.assert;
-
-  mocha.setup("bdd");
-
-  require([
-    "test/spec/index"
-  ], mocha.run);
-});
+import spec from "./spec/index";
